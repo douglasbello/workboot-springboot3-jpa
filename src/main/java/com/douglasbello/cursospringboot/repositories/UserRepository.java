c92932abd1a90557c591b667df002f7444fc29f3
Douglas <douglasbelloalv1@outlook.com>
2023-01-19 13:41:53 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/douglasbello/cursospringboot/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.douglasbello.cursospringboot.repositories;
+
+import com.douglasbello.cursospringboot.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
