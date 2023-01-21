f78e64e0d198a214f6aeaaa9988345393f6dbcf0
napster <douglasbelloalv1@outlook.com>
2023-01-21 01:31:49 -0300

    exception handling - update

--- src/main/java/com/douglasbello/cursospringboot/services/UserService.java
+++ src/main/java/com/douglasbello/cursospringboot/services/UserService.java
@@ -4,6 +4,7 @@ import com.douglasbello.cursospringboot.entities.User;
 import com.douglasbello.cursospringboot.repositories.UserRepository;
 import com.douglasbello.cursospringboot.services.exceptions.DatabaseException;
 import com.douglasbello.cursospringboot.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -41,13 +42,15 @@ public class UserService {
     }
 
     public User update(Long id,User obj) {
-        User entity = userRepository.getReferenceById(id); // ele retorna o obj com o id passado sem acessar o bd, ou
-        // seja, não efetua nenhuma operação com o bd.
+        try {
+            User entity = userRepository.getReferenceById(id); // ele retorna o obj com o id passado sem acessar o bd, ou
+            // seja, não efetua nenhuma operação com o bd.
 
-        updateData(entity,obj);
-
-        return userRepository.save(entity);
-
+            updateData(entity,obj);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
